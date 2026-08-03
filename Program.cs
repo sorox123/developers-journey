04fb7609466932cb6bf9b572d717d106f0adf3ef
Kyle Robert Kalani Allen <sorox123@gmail.com>
2026-08-02 19:37:11 -0700

    Added JSON save file, reading logic, and writing logic

--- Program.cs
+++ Program.cs
@@ -1,20 +1,37 @@
-﻿List<Quest> quests = new List<Quest>(); //defines a new list of the class Quest called "quests"
+﻿using System.Text.Json;
+using System.IO;
 
-Quest quest1 = new Quest(); // the first item of the "quests" list, filled with attributes of Quest class
-quest1.Title = "Ship a small C# application";
-quest1.IsComplete = false;
-quest1.XP = 50;
-quests.Add(quest1);
+List<Quest> quests = new();
+int totalXP = 0;
 
-Quest quest2 = new Quest();
-quest2.Title = "Create a professional README";
-quest2.IsComplete = false;
-quest2.XP = 30;
-quests.Add(quest2);
+if (File.Exists("quests.json"))
+{
+    string json = File.ReadAllText("quests.json"); // checks save file
+    SaveData save = JsonSerializer.Deserialize<SaveData>(json); // passes save file here
+    quests = save.Quests;
+    totalXP = save.TotalXP;
+}
+else
+{
+    Quest quest1 = new Quest(); // the first item of the "quests" list, filled with attributes of Quest class
+    quest1.Title = "Ship a small C# application";
+    quest1.IsComplete = false;
+    quest1.XP = 50;
+    quests.Add(quest1);
+
+    Quest quest2 = new Quest();
+    quest2.Title = "Create a professional README";
+    quest2.IsComplete = false;
+    quest2.XP = 30;
+    quests.Add(quest2);
+
+}
+
+
 
 bool keepPlaying = true;
 
-int totalXP = 0;
+
 
 while (keepPlaying)
 {
@@ -28,7 +45,12 @@ while (keepPlaying)
     try
     {
         int chosenNumber = int.Parse(input); // assigns int chosenNumber to user input
-        Quest chosenQuest = quests[chosenNumber -1]; 
+        Quest chosenQuest = quests[chosenNumber -1];
+        if (chosenQuest.IsComplete)
+        {
+            Console.WriteLine("Quest is already completed.");
+            continue;
+        } 
         int earnedXP = chosenQuest.Complete();
         totalXP = totalXP + earnedXP;
         Console.WriteLine("Total XP: " + totalXP);
@@ -43,12 +65,20 @@ while (keepPlaying)
     string again = Console.ReadLine();
     keepPlaying = (again == "yes");
 }
+// have to redefine a SaveData class object with a different name because I hate myself and I am not using wrappers for top-end objects for some reason.
+SaveData saveOut = new SaveData();
+saveOut.Quests = quests;
+saveOut.TotalXP = totalXP;
+
+
+string data = JsonSerializer.Serialize(saveOut); // write to the save file
+File.WriteAllText("quests.json", data);
 
 class Quest // class Quest consists of its title and completion state, 
 {
-    public string Title;
-    public bool IsComplete;
-    public int XP;
+    public string Title { get; set; }
+    public bool IsComplete { get; set; }
+    public int XP { get; set; }
 
     public int Complete() // sets IsComplete to true, prints message and returns int XP
     {
@@ -56,4 +86,10 @@ class Quest // class Quest consists of its title and completion state,
         Console.WriteLine(Title + " marked complete! +" + XP + " XP");
         return XP;
     }
+}
+// holds both quests and totalxp
+class SaveData
+{
+    public List<Quest> Quests { get; set; }
+    public int TotalXP { get; set; }
 }
